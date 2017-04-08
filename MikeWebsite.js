d80cc42011513e0db481249a68799daff089e230
Kinesum <mike.wilson983@gmail.com>
2017-04-08 15:11:28 -0600

    yree

--- MikeWebsite.js
+++ MikeWebsite.js
@@ -1,3 +1,5 @@
+//This code is from w3schools.com
+
 var slideIndex = 0;
 showSlides();
 
@@ -10,5 +12,5 @@ function showSlides() {
     slideIndex++;
     if (slideIndex> slides.length) {slideIndex = 1} 
     slides[slideIndex-1].style.display = "block"; 
-    setTimeout(showSlides, 4000); // Change image every 4 seconds
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
